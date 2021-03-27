4f5ee4cb88efb96345b6e47e98da951e0701828a
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-03-27 16:19:54 -0400

    heroku stuff

--- server.js
+++ server.js
@@ -7,25 +7,27 @@ let db,
     dbConnectionStr = 'mongodb+srv://michaelizaguirre:OeLoN11gbId0wRSrRKuQ@cluster0.qapjw.mongodb.net/task-list?retryWrites=true&w=majority',
     dbName = 'task-list'
 
-await MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
-            .then(client =>{
-                console.log(`Connected to ${dbName} Database`)
-                db = client.db(dbName)
-                
-            })
+
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 app.use(express.urlencoded({ extended: true}))
 app.use(express.json())
 
-app.get('/',(request, response)=>{
+app.get('/', async (request, response)=>{
+    await MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+            .then(client =>{
+                console.log(`Connected to ${dbName} Database`)
+                db = client.db(dbName)
+                
+            })
     db.collection('tasks').find().toArray()
     .then(data => {
         response.render('index.ejs', { info: data })
     })
     .catch(error => console.error(error))
 })
+
 
 app.post('/addTask', (request, response) => {
     db.collection('tasks').insertOne(request.body)
