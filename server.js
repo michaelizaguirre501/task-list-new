5eaed53faae64cf48aff1c29319e98dd6b160856
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-03-27 16:14:43 -0400

    heroku stuff

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ let db,
     dbConnectionStr = 'mongodb+srv://michaelizaguirre:OeLoN11gbId0wRSrRKuQ@cluster0.qapjw.mongodb.net/task-list?retryWrites=true&w=majority',
     dbName = 'task-list'
 
-MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
+await MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
             .then(client =>{
                 console.log(`Connected to ${dbName} Database`)
                 db = client.db(dbName)
