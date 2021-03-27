f8c396cdd451f76ee52c74df765c0b391ad4c96d
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-03-27 16:33:52 -0400

    heroku working

--- server.js
+++ server.js
@@ -7,48 +7,56 @@ let db,
     dbConnectionStr = 'mongodb+srv://michaelizaguirre:OeLoN11gbId0wRSrRKuQ@cluster0.qapjw.mongodb.net/task-list?retryWrites=true&w=majority',
     dbName = 'task-list'
 
+MongoClient.connect(dbConnectionStr, {
+        useUnifiedTopology: true
+    })
+    .then(client => {
+        console.log(`Connected to ${dbName} Database`)
+        db = client.db(dbName)
+
+    })
 
 
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true}))
+app.use(express.urlencoded({
+    extended: true
+}))
 app.use(express.json())
 
-app.get('/', async (request, response)=>{
-    await MongoClient.connect(dbConnectionStr, {useUnifiedTopology: true})
-            .then(client =>{
-                console.log(`Connected to ${dbName} Database`)
-                db = client.db(dbName)
-                
-            })
+app.get('/', (request, response) => {
     db.collection('tasks').find().toArray()
-    .then(data => {
-        response.render('index.ejs', { info: data })
-    })
-    .catch(error => console.error(error))
+        .then(data => {
+            response.render('index.ejs', {
+                info: data
+            })
+        })
+        .catch(error => console.error(error))
 })
 
 
 app.post('/addTask', (request, response) => {
     db.collection('tasks').insertOne(request.body)
-    .then(result => {
-        console.log('Task Added')
-        response.redirect('/')
-    })
-    .catch(error => console.error(error))
+        .then(result => {
+            console.log('Task Added')
+            response.redirect('/')
+        })
+        .catch(error => console.error(error))
 })
 
 app.delete('/deleteTask', (request, response) => {
-    db.collection('tasks').deleteOne({taskName: request.body.taskNameS})
-    .then(result => {
-        console.log('Task Deleted')
-        response.json('Task Deleted')
-    })
-    .catch(error => console.error(error))
+    db.collection('tasks').deleteOne({
+            taskName: request.body.taskNameS
+        })
+        .then(result => {
+            console.log('Task Deleted')
+            response.json('Task Deleted')
+        })
+        .catch(error => console.error(error))
 
 })
 
 
-app.listen(process.env.PORT || PORT, ()=>{
+app.listen(process.env.PORT || PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
