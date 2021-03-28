da767b2a2e3342bd4ccdae32dbcfad76338c1cd7
Michael Izaguirre <michael.izaguirre501@gmail.com>
2021-03-27 21:30:04 -0400

    db change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const MongoClient = require('mongodb').MongoClient
 const PORT = 8000
 
 let db,
-    dbConnectionStr = 'mongodb+srv://michaelizaguirre:OeLoN11gbId0wRSrRKuQ@cluster0.qapjw.mongodb.net/task-list?retryWrites=true&w=majority',
+    dbConnectionStr = 'mongodb+srv://michaelizaguirre:tasklistdemo@cluster0.qapjw.mongodb.net/task-list?retryWrites=true&w=majority',
     dbName = 'task-list'
 
 MongoClient.connect(dbConnectionStr, {
